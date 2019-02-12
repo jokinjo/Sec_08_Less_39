e9d163ddb6116956f5a71684f9da404ea97befee
JMS <jokinjo2001@hotmail.com>
2019-02-12 08:40:08 -0500

    Tick-Tack-Toe game is a Success  - Include restart capability.

--- app/src/main/java/com/example/sec_08_less_39/MainActivity.java
+++ app/src/main/java/com/example/sec_08_less_39/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sec_08_less_39;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.support.v7.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -10,43 +12,56 @@ public class MainActivity extends AppCompatActivity {
 
     enum Player {ONE, TWO}
     Player currentPlayer = Player.ONE;
-    Player mPlayer;
     String strWinner = "", string = "";
     Integer row=0, col=0, sum=0;
     int[][] RC = new int[3][3];
-    boolean isGameOver =false;
+    boolean isGameOver = false;
+    Button btnRestart;
+    GridLayout mGridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRestart = findViewById(R.id.btnRestart);
+        mGridLayout = findViewById(R.id.gridLayout);
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restartGame();
+            }
+        });
     }
     public void gameOver(Player mPlayer) {
         if (mPlayer == Player.ONE) strWinner = "TIGER WINS!";
         if (mPlayer == Player.TWO) strWinner = "LION WINS!";
         Toast.makeText( MainActivity.this, strWinner, Toast.LENGTH_LONG).show();
         isGameOver = true;
-    };
+        btnRestart.setVisibility(View.VISIBLE);
+    }
 
     public void imageViewIsTapped(View imgView) {
         if (!isGameOver) {
             ImageView tappedImageView = (ImageView) imgView;
             tappedImageView.setTranslationX(-1000);
-            tappedImageView.animate().alphaBy(1.0f).translationXBy(1000f).rotationXBy(360*5f).setDuration(2000);
+            tappedImageView.animate().alpha(1.0f).translationXBy(1000f).rotationXBy(360*5f).setDuration(2000);
 
-            string = String.valueOf(tappedImageView.getId());
+            string = String.valueOf(tappedImageView.getTag());
             switch (string) {
-                case "2131165265": row = 0; col = 0; break;
-                case "2131165264": row = 0; col = 1; break;
-                case "2131165263": row = 0; col = 2; break;
-                case "2131165262": row = 1; col = 0; break;
-                case "2131165261": row = 1; col = 1; break;
-                case "2131165260": row = 1; col = 2; break;
-                case "2131165259": row = 2; col = 0; break;
-                case "2131165258": row = 2; col = 1; break;
-                case "2131165257": row = 2; col = 2; break;
-                default: Toast.makeText(MainActivity.this, String.valueOf(tappedImageView.getId()), Toast.LENGTH_LONG).show();
+                case "9": row = 0; col = 0; break;
+                case "8": row = 0; col = 1; break;
+                case "7": row = 0; col = 2; break;
+                case "6": row = 1; col = 0; break;
+                case "5": row = 1; col = 1; break;
+                case "4": row = 1; col = 2; break;
+                case "3": row = 2; col = 0; break;
+                case "2": row = 2; col = 1; break;
+                case "1": row = 2; col = 2; break;
+                default: Toast.makeText(MainActivity.this, String.valueOf(tappedImageView.getTag()), Toast.LENGTH_LONG).show();
             }
+
             if (currentPlayer == Player.ONE) {
                 tappedImageView.setImageResource(R.drawable.tiger);
                 currentPlayer = Player.TWO;
@@ -74,6 +89,23 @@ public class MainActivity extends AppCompatActivity {
                 if (sum == -3) gameOver(Player.ONE);
                 if (sum == +3) gameOver(Player.TWO);
             }
+        }
+    }
+    public void restartGame() {
+        currentPlayer = Player.ONE;
+        strWinner = "";
+        string = "";
+        row = 0;
+        col = 0;
+        sum = 0;
+        isGameOver = false;
+        btnRestart.setVisibility(View.GONE);
+        for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) RC[i][j] = 0;
+
+        for (int index = 0; index < mGridLayout.getChildCount(); index++) {
+            ImageView imageView = (ImageView) mGridLayout.getChildAt(index);
+            imageView.setImageDrawable(null);
+            imageView.setAlpha(0.2f);
         }
     }
 }
